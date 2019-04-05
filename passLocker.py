ffccfc8df34699abb4ba61e08dddd08eccf42547
AugustineOchieng <gusochieng@gmail.com>
2019-04-05 16:13:34 +0300

    delete unwanted files

--- passLocker.py
+++
@@ -1,9 +0,0 @@
-class User:
-  user_details = []
-  def __init__(self, first_name, last_name, phone_number, email_address, username, password):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.phone_number = phone_number
-    self.email_address = email_address
-    self.username = username
-    self.password = password
