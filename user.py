7bbf5f68e8b9960bdff9fbf8d2111beadac3ed18
AugustineOchieng <gusochieng@gmail.com>
2019-04-05 16:12:04 +0300

    redefined class

--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 class User:
   user_details = []
+
   def __init__(self, first_name, last_name, phone_number, email_address, username, password):
     self.first_name = first_name
     self.last_name = last_name
