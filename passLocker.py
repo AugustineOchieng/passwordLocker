0eed7750f07676c1bd176d2657d232d061f447be
AugustineOchieng <gusochieng@gmail.com>
2019-04-05 15:23:24 +0300

    add class definition

--- passLocker.py
+++ passLocker.py
@@ -1,4 +1,9 @@
-class Credentials:
+class User:
+  user_details = []
   def _init_(self, first_name, last_name, phone_number, email_address, username, password):
     self.first_name = first_name
-    self.last_name = 
+    self.last_name = last_name
+    self.phone_number = phone_number
+    self.email_address = email_address
+    self.username = username
+    self.password = password
