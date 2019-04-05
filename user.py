2f09816726f3a78bba560656cd9498b753fee2f8
AugustineOchieng <gusochieng@gmail.com>
2019-04-05 17:45:26 +0300

    modify user.py

--- user.py
+++ user.py
@@ -1,7 +1,8 @@
 class User:
     user_details = []
 
-    def __init__(self, first_name, last_name, phone_number, email_address, username, password):
+    def __init__(self, account, first_name, last_name, phone_number, email_address, username, password):
+        self.account = account
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
--- user_test.py
+++ user_test.py
@@ -4,9 +4,10 @@ from user import User
 
 class TestUser(unittest.TestCase):
     def setUp(self):
-        self.new_user = User("Gus", "Will", "0712345678","gus@gmail.com", "gus", "gus00")
+        self.new_user = User("Twitter","Gus", "Will", "0712345678","gus@gmail.com", "gus", "gus00")
 
     def test_init(self):
+        self.assertEqual(self.new_user.account, "Twitter")
         self.assertEqual(self.new_user.first_name, "Gus")
         self.assertEqual(self.new_user.last_name, "Will")
         self.assertEqual(self.new_user.phone_number, "0712345678")
@@ -18,14 +19,14 @@ class TestUser(unittest.TestCase):
         self.assertEqual(len(User.user_details), 1)
     def test_save_multiple_contact(self):
         self.new_user.save_user()
-        test_user = User("Gus", "Will", "0712345678","gus@gmail.com", "gus", "gus00")
+        test_user = User("Twitter","Gus", "Will", "0712345678","gus@gmail.com", "gus", "gus00")
         test_user.save_user()
         self.assertEqual(len(User.user_details), 2)
     def tearDown(self):
         User.user_details = []
     def test_save_multiple_user(self):
         self.new_user.save_user()
-        test_user = User("Gus", "Will", "0712345678", "gus@gmail.com", "gus", "gus00")
+        test_user = User("Twitter","Gus", "Will", "0712345678", "gus@gmail.com", "gus", "gus00")
         test_user.save_user()
         self.assertEqual(len(User.user_details), 2)
         
