ecd56aa2fdc0ab196e8afc0110700a522f20efeb
AugustineOchieng <gusochieng@gmail.com>
2019-04-08 08:58:34 +0300

    edit login function

--- run.py
+++ run.py
@@ -77,21 +77,22 @@ while True:
 
                     if short_code == 'cc':
                               while True:
-                              username = input("Username: ")
+                               username = input("Username: ")
                               if not len(username) > 0:
-                              print("Username can't be blank")
-                              else:
+                               print("Username can't be blank")
+                    else:
                               break
                               while True:
-                               password = input("Password: ")
-                               if not len(password) > 0:
-                                print("Password can't be blank")
-                              else:
-                             break
+                                password = input("Password: ")
+                              if not len(password) > 0:
+                               print("Password can't be blank")
+                   
+else:
+                              break
 
                               if loginauth(username, password):
-                              return session(username)
-                             else:
+                              return (username)
+                    else:
                             print("Invalid username or password")
 
                             print("New Account")
